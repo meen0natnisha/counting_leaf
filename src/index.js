e7ddb2650b06a2da7f3c13e5a17b3ece721b94c6
ZShiroeSamaZ <47985474+ZShiroeSamaZ@users.noreply.github.com>
2022-03-21 15:14:21 +0700

    add serve, delete start command in package

--- src/index.js
+++ src/index.js
@@ -1,18 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import axios from 'axios';
-axios.defaults.baseURL = 
-// "http://localhost:5001/leaf-counting/asia-southeast1/api";
-"https://asia-southeast1-leaf-counting.cloudfunctions.net/api";
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import axios from "axios";
+axios.defaults.baseURL =
+  "https://asia-southeast1-leaf-counting.cloudfunctions.net/api";
+
+if (
+  window.location.hostname === "localhost" ||
+  window.location.hostname === "127.0.0.1" ||
+  window.location.hostname === ""
+)
+  axios.defaults.baseURL =
+    "http://localhost:5001/leaf-counting/asia-southeast1/api";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
